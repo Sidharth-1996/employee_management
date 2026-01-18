044f911047faaf96e11a085092246bdb1c39367e
Sidharth-1996 <sharmasidharth784@gmail.com>
2026-01-19 00:02:02 +0530

    create suoeruser after migrations using signal

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from django.contrib.auth import get_user_model
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,25 +130,3 @@ if not DEBUG:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
 
-
-def create_superuser():
-    username = os.getenv("DJANGO_SUPERUSER_USERNAME")
-    email = os.getenv("DJANGO_SUPERUSER_EMAIL")
-    password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
-
-    if not all([username, email, password]):
-        return
-
-    User = get_user_model()
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password,
-        )
-        print("Superuser created")
-
-try:
-    create_superuser()
-except Exception as e:
-    print("Superuser creation skipped:", e)
